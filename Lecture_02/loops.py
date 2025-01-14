6f3ace564676dc1e47535754b27d9219372b119d
Heyeyeyey-36 <danikiss@gmail.comgit config --global user.email danikiss@gmail.comgit config --global user.email danikiss@gmail.com>
2025-01-14 10:51:47 +0100

    loops

---
+++ Lecture_02/loops.py
@@ -0,0 +1,21 @@
+#i = 10
+
+#while i > 0:
+#      i = i-1
+
+
+#while True:
+ #     if i > 0 :
+ #       i= i-1
+ #     else:
+ #          break
+      
+
+i = 0
+j = 0
+
+while (i+j < 20):
+     i = i + 3 
+     j = j + 1
+
+print (i, j) 
