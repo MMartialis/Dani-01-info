cd976a07f636e4c209a814a98ecdec245df0ef1b
Heyeyeyey-36 <danikiss@gmail.comgit config --global user.email danikiss@gmail.comgit config --global user.email danikiss@gmail.com>
2025-01-14 18:15:40 +0100

    fasy

---
+++ C++/Arrays/arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+    int cic[4];
+    cic[1] = 2;
+    cic[2] = 4;
+    cic[3] = 6;
+    printf("Firts number is: %d\n", cic[1]);
+
+    char kuty[7];
+    kuty[1] = 'k';
+    kuty[2] = 'u';
+    kuty[3] = 't';
+    kuty[4] = 'y';
+    kuty[5] = 'u';
+    kuty[6] = 's';
+    kuty[7]= '\0';
+    printf("The word is:%s", kuty);
+
+}
---
+++ C++/Function/function.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+float addnumb(float a, float b){
+    return a+b;
+}
+
+void main(){
+    printf("The addded number is: %f\n %d", addnumb(1.1, 2.2), 2);
+    }
