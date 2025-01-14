4c8d0a52f816565c88f57dbd8b2d1f0a6d3a33a0
Heyeyeyey-36 <danikiss@gmail.comgit config --global user.email danikiss@gmail.comgit config --global user.email danikiss@gmail.com>
2025-01-14 16:41:04 +0100

    Recursive

--- Lecture_03/recursive.py
+++ Lecture_03/recursive.py
@@ -1,9 +1,46 @@
-def factorial(a:int)->int:
-    if a <= 0:
-        return 0
-    return(a + factorial(a-1))
+#def factorial(a:int)->int:
+#    if a <= 0:
+#        return 0
+#    return(a + factorial(a-1))
+
+#print("The fact is: ", factorial(4) )
+
+
+#def fib(a:int)->int:
+#    if a == 1 or a == 2: 
+#        return 1
+#    return(fib(a-1) + fib(a-2))
+
+#print("The 10th element is:", fib(10))
+
+
+#def pir(a:int)->int:
+#    if a <= 0:
+#        return 0
+#    return(a + pir(a-1))
+
+#h=5
+#for i in range(h):
+#    for _ in range(i):
+#        print(" ", end="")
+#    for _ in range(((h-i)*2)-1):
+#        print("⬤", end="")
+#    print()
 
 
 
+def recpir(h:int, s:int=1):
+    if s <= h:
+        for _ in range(s-1):
+            print(" ", end="")
 
-print("The fact is: ", factorial(4) )
+        for _ in range((h-(s-1))*2-1):
+            print("⬤", end="")
+        print() 
+        recpir(h, s+1)
+    else:
+       return False 
+
+
+
+recpir (5)
