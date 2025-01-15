2c60d19bbebfc7d0a2023c520ddc7d2c0df65e33
Heyeyeyey-36 <danikiss@gmail.comgit config --global user.email danikiss@gmail.comgit config --global user.email danikiss@gmail.com>
2025-01-15 10:18:43 +0100

    pointer global

---
+++ C++/ref-pointers/pointers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void passByRef(int* numberaddress){
+    printf("Global variable is stored at: %p \n", numberaddress); 
+    printf("Global variable is: %d \n", *numberaddress); 
+    *numberaddress = *numberaddress *2; 
+
+}
+
+int passByValueI(int x){
+    x = x*2;
+    return x; 
+}
+
+
+void main(){
+    // int n = 5;
+    // printf("The number is : %d \n", n);
+
+    // int* address = &n;
+    //  printf("The numbers address is : %p", address); 
+
+    // printf("The number stored at address %p is : %d \n", address, *address); 
+
+
+    // int cicus;
+    // printf("How many cicus are there?: ");
+    // scanf("%d", &cicus);
+    // printf("There are %d kittens.", cicus);
+
+
+    int numb = 10;
+    printf("The number is: %d \n", numb);
+    passByRef(&numb);
+    printf("The number after the function ran is: %d \n", numb);
+
+    // printf("Now the number is %d ", passByValueI(numb)); 
+    // numb = passByValueI(numb);
+    // printf("Now the number is %d ", numb); 
+
+}
